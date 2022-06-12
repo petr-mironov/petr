ed486bd979afa34c34858ac15906cab03e1b55d5
petr mironov <mironov.petka@mail.ru>
2022-06-12 13:12:53 +0300

    add Example 005 Condition If-Els

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine(result);
+Example005_Console.WriteLine(result);
